c918960f9e6a9ef0c81a39324f37bf2b2ee7a452
ksion4ik1 <ksion4ik.life@gmail.com>
2021-09-29 02:32:18 +0300

    add new fils

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// ===========================================changebackground===========================================
+$(function(){  
+  var i =0;
+  var images = ['../img/imeg0.jpg','../img/imeg1.jpg','../img/imeg2.jpg','../img/imeg3.png','../img/imeg4.png','../img/imeg5.jpg'];
+  var image = $('#sectionTablo');
+  // image.css('background-image', 'url(/img/imeg1.jpg)');
+  setInterval(function(){  
+      image.fadeOut(1000, function () {
+          image.css('background-image', 'url(' + images[i] +')');
+          image.fadeIn(1000);
+      });
+      if(i == (images.length - 1)){
+          i = 0; 
+      } else {
+          i++;
+      }
+  }, 20000);
+})
+// =============================================================================================
 function opentab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -8,7 +27,7 @@ function opentab(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(cityName).style.display = "flex";
     evt.currentTarget.className += " active";
   }
   // =====================Slider===================
@@ -41,10 +60,17 @@ function opentab(evt, cityName) {
           slidesToShow: 1,
           slidesToScroll: 1
         }
-      }
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
     ]
+  });
+  $('.StoreDuty').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
   });
   
